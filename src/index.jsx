df514a3c65359ba7b826cb81d8cfa6caca95730e
Brandon Horst <brandonhorst@gmail.com>
2015-12-15 09:03:06 -0500

    Add demo functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 /** @jsx createElement */
 
+import _ from 'lodash'
 import {createElement, Phrase} from 'lacona-phrase'
 import String from 'lacona-phrase-string'
 import PhoneNumber from 'lacona-phrase-phonenumber'
@@ -7,26 +8,30 @@ import EmailAddress from 'lacona-phrase-email'
 
 export function execute (result) {
   let url
+  const to = encodeURIComponent(_.chain(result.to).map('value').join(',').value())
 
-  if (result.email) {
-    if (result.email.message) {
-      url = `mailto:${encodeURIComponent(result.email.to.join(','))}?subject=${result.email.subject}`
+  if (result.verb === 'email') {
+    if (result.to) {
+      url = `mailto:${to}?subject=${result.subject}`
     } else {
-      url = `mailto:${encodeURIComponent(result.email.to.join(','))}`
+      url = `mailto:${to}`
     }
-  } else if (result.call) {
-    url = `tel://${encodeURIComponent(result.call.number[0])}`
-  } else if (result.facetime) {
-    url = `facetime://${encodeURIComponent(result.facetime.contact[0])}`
-  } else if (result.text) {
-    url = `imessage://${encodeURIComponent(result.text.contact.join(','))}`
+  } else if (result.verb === 'call') {
+    url = `tel://${to}`
+  } else if (result.verb === 'facetime') {
+    url = `facetime://${to}`
+  } else if (result.verb === 'text') {
+    url = `imessage://${to}`
   }
 
-  console.log(url)
   global.openURL(url)
 }
 
 class EmailGroup extends Phrase {
+  getValue (result) {
+    return result.map(email => ({label: 'email', value: email}))
+  }
+
   describe () {
     return (
       <repeat unique={true} separator={<list items={[' and ', ', and ', ', ']} limit={1} />}>
@@ -37,6 +42,10 @@ class EmailGroup extends Phrase {
 }
 
 class NumberGroup extends Phrase {
+  getValue (result) {
+    return result.map(email => ({label: 'number', value: email}))
+  }
+
   describe () {
     return (
       <repeat unique={true} separator={<list items={[' and ', ', and ', ', ']} limit={1} max={this.props.max} />}>
@@ -47,12 +56,22 @@ class NumberGroup extends Phrase {
 }
 
 class AllGroup extends Phrase {
+  getValue (result) {
+    return result.map(item => {
+      if (item.number) {
+        return {label: 'number', value: item.number}
+      } else if (item.email) {
+        return {label: 'email', value: item.email}
+      }
+    })
+  }
+
   describe () {
     return (
       <repeat unique={true} separator={<list items={[' and ', ', and ', ', ']} limit={1} max={this.props.max} />}>
         <choice>
-          <PhoneNumber />
-          <EmailAddress />
+          <PhoneNumber id='number' />
+          <EmailAddress id='email' />
         </choice>
       </repeat>
     )
@@ -63,56 +82,48 @@ export class Sentence extends Phrase {
   describe () {
     return (
       <choice>
-        <choice id='email'>
-          <sequence>
-            <list items={['email ', 'send an email to ', 'send email to ', 'shoot an email to ']} category='action' limit={1} />
-            <EmailGroup id='to' />
-          </sequence>
-          <sequence>
-            <list items={['send ']} category='action' limit={1} />
-            <EmailGroup id='to' />
-            <literal text=' an email' />
-          </sequence>
-          <sequence>
-            <list items={['email ', 'send ']} category='action' limit={1}/>
-            <String argument='subject' id='subject' limit={1} />
-            <literal text=' to ' category='conjunction' />
-            <EmailGroup id='to' />
-          </sequence>
-          <sequence>
-            <list items={['email ', 'send an email to ', 'send email to ', 'shoot an email to ']} category='action' limit={1} />
-            <EmailGroup id='to' />
-            <choice limit={1}>
-              <literal text=' about ' />
-              <literal text=' ' />
-            </choice>
-            <String argument='subject' id='subject' limit={1} />
-          </sequence>
-          <sequence>
-            <list items={['send ']} category='action' limit={1} />
-            <EmailGroup id='to' />
-            <choice limit={1} category='action'>
-              <literal text=' an email about ' />
-              <literal text=' an email ' />
-              <literal text=' email about ' />
-              <literal text=' email ' />
-            </choice>
-            <String argument='subject' id='subject' limit={1} />
-          </sequence>
-        </choice>
-        <sequence id='call'>
-          <list items={['call ', 'ring ', 'call up ', 'ring up ']} category='action' limit={1} />
-          <NumberGroup id='number' max={1} />
+        <sequence>
+          <list items={['email ', 'send an email to ', 'send email to ', 'shoot an email to ']} category='action' id='verb' value='email' limit={1} />
+          <EmailGroup id='to' />
         </sequence>
-        <sequence id='facetime'>
-          <literal text='facetime ' category='action' />
-          <AllGroup merge={true} max={1} id='contact' />
+        <sequence>
+          <list items={['send ']} id='verb' value='email' category='action' limit={1} />
+          <EmailGroup id='to' />
+          <literal text=' an email' />
         </sequence>
-        <choice id='text'>
-          <sequence>
-            <list items={['text ', 'iMessage ', 'shoot a text to ', 'send a text to ']} limit={1} category='action' />
-            <AllGroup id='contact' />
-          </sequence>
+        <sequence>
+          <list items={['email ', 'send ']} id='verb' value='email' category='action' limit={1}/>
+          <String argument='subject' id='subject' limit={1} />
+          <literal text=' to ' category='conjunction' />
+          <EmailGroup id='to' />
+        </sequence>
+        <sequence>
+          <list items={['email ', 'send an email to ', 'send email to ', 'shoot an email to ']} id='verb' value='email' category='action' limit={1} />
+          <EmailGroup id='to' />
+          <choice limit={1}>
+            <literal text=' about ' />
+            <literal text=' ' />
+          </choice>
+          <String argument='subject' id='subject' limit={1} />
+        </sequence>
+        <sequence>
+          <list items={['send ']} category='action' limit={1} />
+          <EmailGroup id='to' />
+          <list limit={1} category='action' items={[' an email about ', ' an email ', ' email about ', ' email ']} id='verb' value='email' />
+          <String argument='subject' id='subject' limit={1} />
+        </sequence>
+        <sequence>
+          <list items={['call ', 'ring ', 'call up ', 'ring up ']} category='action' limit={1} id='verb' value='call' />
+          <NumberGroup id='to' max={1} />
+        </sequence>
+        <sequence>
+          <literal text='facetime ' category='action' id='verb' value='facetime' />
+          <AllGroup max={1} id='to' />
+        </sequence>
+        <sequence>
+          <list items={['text ', 'iMessage ', 'shoot a text to ', 'send a text to ']} limit={1} category='action' id='verb' value='text' />
+          <AllGroup id='to' />
+        </sequence>
           {/* <sequence>
             <list items={['text ', 'iMessage ']} limit={1} category='action' />
             <String argument='message' id='message' limit={1} />
@@ -139,7 +150,6 @@ export class Sentence extends Phrase {
             </choice>
             <String argument='message' id='message' limit={1} />
           </sequence>*/}
-        </choice>
       </choice>
     )
   }
