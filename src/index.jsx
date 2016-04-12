f2a2b2b5f1ec1ab9ab7766f0867c8e9b0c28c0c8
Brandon Horst <brandonhorst@gmail.com>
2016-04-11 23:18:13 -0400

    Add facetime audio, clean up

--- src/index.jsx
+++ src/index.jsx
@@ -98,6 +98,8 @@ export const Communicate = {
       url = `tel://${urlify(result.to)}`
     } else if (result.verb === 'facetime') {
       url = `facetime://${urlify(result.to)}`
+    } else if (result.verb === 'facetime-audio') {
+      url = `facetime-audio://${urlify(result.to)}`
     } else if (result.verb === 'text') {
       url = `imessage://${urlify(result.to, item => item.replace(/[\(\)\s-]/g, ''))}`
     }
@@ -108,58 +110,74 @@ export const Communicate = {
   describe () {
     return (
       <choice>
-        <sequence>
-          <list items={['email ', 'send an email to ', 'send email to ', 'shoot an email to ']} category='action' id='verb' value='email' limit={1} />
-          <EmailGroup id='to' />
-        </sequence>
-        <sequence>
-          <list items={['send ']} id='verb' value='email' category='action' limit={1} />
-          <EmailGroup id='to' />
-          <literal text=' an email' />
-        </sequence>
-        <sequence>
-          <list items={['email ', 'send ']} id='verb' value='email' category='action' limit={1}/>
-          <String argument='subject' id='subject' limit={1} />
-          <literal text=' to ' category='conjunction' />
-          <EmailGroup id='to' />
-        </sequence>
-        <sequence>
-          <list items={['email ', 'send an email to ', 'send email to ', 'shoot an email to ']} id='verb' value='email' category='action' limit={1} />
-          <EmailGroup id='to' />
-          <choice limit={1}>
+        <choice limit={1}>
+          <sequence>
+            <list items={['send ', 'shoot ']} limit={1} id='verb' value='email' />
+            <EmailGroup id='to' />
+            <literal text=' an email' ellipsis />
             <literal text=' about ' />
-            <literal text=' ' />
-          </choice>
-          <String argument='subject' id='subject' limit={1} />
-        </sequence>
-        <sequence>
-          <list items={['send ']} category='action' limit={1} />
-          <EmailGroup id='to' />
-          <list limit={1} category='action' items={[' an email about ', ' an email ', ' email about ', ' email ']} id='verb' value='email' />
-          <String argument='subject' id='subject' limit={1} />
-        </sequence>
-        <sequence>
-          <list items={['call ', 'ring ', 'call up ', 'ring up ']} category='action' limit={1} id='verb' value='call' />
-          <NumberGroup id='to' max={1} />
-        </sequence>
-        <sequence>
-          <literal text='facetime ' category='action' id='verb' value='facetime' />
-          <AllGroup max={1} id='to' />
-        </sequence>
-        <sequence>
-          <list items={['text ', 'iMessage ', 'shoot a text to ', 'send a text to ']} limit={1} category='action' id='verb' value='text' />
-          <AllGroup id='to' />
-        </sequence>
+            <String argument='subject' id='subject' limit={1} />
+          </sequence>
+          <sequence>
+            <list items={['email ', 'send an email to ', 'send email to ', 'shoot an email to ']} id='verb' value='email' limit={1} />
+            <EmailGroup id='to' ellipsis />
+            <list items={[' about ', ' ']} limit={1} />
+            <String argument='subject' id='subject' limit={1} />
+          </sequence>
+          <sequence>
+            <literal text='email ' id='verb' value='email' />
+            <String argument='subject' id='subject' limit={1} />
+            <literal text=' to ' />
+            <EmailGroup id='to' />
+          </sequence>
+        </choice>
+        <choice limit={1}>
+          <sequence>
+            <list items={['call ']} limit={1} id='verb' value='call' />
+            <NumberGroup id='to' max={1} />
+          </sequence>
+          <sequence>
+            <list items={['give ']} limit={1} id='verb' value='call' />
+            <NumberGroup id='to' max={1} />
+            <list items={[' a call ', ' a ring']} limit={1} id='verb' value='call' />
+          </sequence>
+        </choice>
+        <choice limit={1}>
+          <sequence>
+            <literal text='facetime ' id='verb' value='facetime' />
+            <literal text='audio ' id='verb' value='facetime-audio' optional />
+            <AllGroup max={1} id='to' />
+          </sequence>
+          <sequence>
+            <literal text='give ' id='verb' value='facetime' />
+            <AllGroup max={1} id='to' />
+            <choice id='verb'>
+              <list items={[' a facetime call']} value='facetime' />
+              <list items={[' a facetime audio call']} value='facetime-audio' />
+            </choice>
+          </sequence>
+        </choice>
+        <choice limit={1}>
+          <sequence>
+            <list items={['send ', 'shoot ']} limit={1} id='verb' value='text' />
+            <AllGroup id='to' />
+            <list items={[' a text', ' an iMessage']} limit={1} id='verb' value='text' />
+          </sequence>
+          <sequence>
+            <list items={['text ', 'iMessage ', 'send a text to ', 'shoot a text to ']} limit={1} id='verb' value='text' />
+            <AllGroup id='to' />
+          </sequence>
+        </choice>
           {/* <sequence>
-            <list items={['text ', 'iMessage ']} limit={1} category='action' />
+            <list items={['text ', 'iMessage ']} limit={1} />
             <String argument='message' id='message' limit={1} />
-            <literal text=' to ' category='conjunction' />
+            <literal text=' to ' />
             <AllGroup merge={true} />
           </sequence>
           <sequence>
-            <list items={['send ']} limit={1} category='action' />
+            <list items={['send ']} limit={1} />
             <AllGroup merge={true} />
-            <choice limit={1} category='action'>
+            <choice limit={1} 
               <literal text=' a text saying ' />
               <literal text=' an iMessage saying' />
               <literal text=' a text ' />
@@ -168,7 +186,7 @@ export const Communicate = {
             <String argument='message' id='message' limit={1} />
           </sequence>
           <sequence>
-            <list items={['text ', 'iMessage ']} limit={1} category='action' />
+            <list items={['text ', 'iMessage ']} limit={1} />
             <AllGroup merge={true} />
             <choice limit={1}>
               <literal text=' saying ' />
